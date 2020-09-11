4e25a8a9d980cb1a149faca8b6465f877098ad1d
Roman <golo123456@mail.ru>
2020-09-11 14:57:27 +0300

    1_2

--- prak1/01/upr1/pr1-1/pr1-1/Program.cs
+++ prak1/01/upr1/pr1-1/pr1-1/Program.cs
@@ -15,17 +15,18 @@ namespace pr1_2
     public string firstName;
     public string lastName;
     public int age;
-    public Genders gender;
     public enum Genders : int { Male, Female };
+    public Genders gender;
     public Person(string _firstName, string _lastName, int _age, Genders _gender)
     {
       firstName = _firstName;
-      lastName = _lastName; age = _age;
+      lastName = _lastName; 
+      age = _age;
       gender = _gender;
     }
     public override string ToString()
     {
-      return firstName + " " + lastName + "(" + gender + "), + , age " + age;
+      return firstName + " " + lastName + " (" + gender + "), age " + age;
     }
   }
 }
