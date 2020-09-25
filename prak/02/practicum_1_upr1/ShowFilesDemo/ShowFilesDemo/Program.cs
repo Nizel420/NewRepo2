ce63b5e04cb6933b3d8f1456a8a66f52f6bcceb0
Роман КУдаков <nastuwa9090@mail.ru>
2020-09-25 23:02:27 +0300

    4

---
+++ prak/02/practicum_1_upr1/ShowFilesDemo/ShowFilesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System;
+namespace ShowFilesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory); ShowDirectory(dir);
+        }
+
+        static void ShowDirectory(DirectoryInfo dir)
+        {
+            // Показать все файлы
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine("File: {0}", file.FullName);
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    ShowDirectory(subDir);
+                }
+            }
+        }
+    }
+}
---
+++ prak/02/practicum_3_upr1/CompressionDemo/CompressionDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+namespace CompressionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompressFile(@"C:\Users\nastu\Desktop\Новый текстовый документ.txt", @"C:\Users\nastu\Desktop\Новый текстовый документ.txt.gz");
+            UncompressFile(@"C:\Users\nastu\Desktop\Новый текстовый документ.txt.gz", @"C:\Users\nastu\Desktop\Новый текстовый документ1.txt");
+        }
+        static void CompressFile(string inFilename,string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+            
+            GZipStream compStream = new GZipStream(destFile, CompressionMode.Compress);
+            int theByte = sourceFile.ReadByte();
+            while (theByte != -1)
+            {
+                compStream.WriteByte((byte)theByte); 
+                theByte = sourceFile.ReadByte();
+            }
+            compStream.Close();
+        }
+        static void UncompressFile(string inFilename, string outFilename)
+        {
+            FileStream sourceFile = File.OpenRead(inFilename);
+            FileStream destFile = File.Create(outFilename);
+            GZipStream compStream = new GZipStream(sourceFile, CompressionMode.Decompress);
+            int theByte = compStream.ReadByte();
+            while (theByte != -1)
+            {
+                destFile.WriteByte((byte)theByte); theByte = compStream.ReadByte();
+            }
+            compStream.Close();
+        }
+
+
+    }
+}
