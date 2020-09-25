29842dae8d9295066e9520416971071756db6bf9
Роман КУдаков <nastuwa9090@mail.ru>
2020-09-25 20:51:39 +0300

    3

---
+++ prak/01/practicum_4_upr_1/type_convertion/type_convertion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace type_convertion
+{
+   // функция изменения параметров компилятора у меня недоступна 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1; Int32 i32 = 1; double db = 1;
+            i16 = 132;
+            i16 = db;
+            i32 = i16;
+            i32 = db;
+            db = 116; db = i32;
+
+        }
+    }
+}
