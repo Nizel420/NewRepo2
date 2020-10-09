7ee561dcfad9caadfc53853d4dc8334a122805c5
Роман КУдаков <nastuwa9090@mail.ru>
2020-10-09 19:13:26 +0300

    09.10

---
+++ prak/03/practitum_2_upr1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\nastu\Desktop\info.ini");
+            StreamWriter sw = new StreamWriter(@"C:\Users\nastu\Desktop\info.txt", false, Encoding.UTF7);
+            sw.WriteLine(sr.ReadToEnd());
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
